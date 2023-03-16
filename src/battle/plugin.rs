9ab4315508e125dce62e7c006abf91c2125c5150
Scott Lambert <s-lambert@live.com.au>
2023-03-16 21:49:38 +0800

    Setup attack code when enemies and players are in range

--- src/battle/plugin.rs
+++ src/battle/plugin.rs
@@ -1,10 +1,13 @@
 use std::time::Duration;
 
-use bevy::{prelude::*, time::common_conditions::on_timer};
+use bevy::{prelude::*, time::common_conditions::on_timer, utils::HashMap};
 
 use crate::{GameState, Position};
 
 pub struct BattlePlugin;
+
+#[derive(Resource, Deref, DerefMut)]
+struct Enemies(HashMap<Position, Entity>);
 
 #[derive(Component)]
 struct Player;
@@ -14,13 +17,13 @@ struct Enemy;
 
 fn place_characters(mut commands: Commands, asset_server: Res<AssetServer>) {
     let mut camera = Camera2dBundle::default();
-    camera.transform.scale = Vec3::new(0.25, 0.25, 1.0);
+    camera.transform.scale = Vec3::new(1.0 / 3.0, 1.0 / 3.0, 1.0);
     commands.spawn(camera);
 
     commands.spawn((
         Player,
         Position {
-            x: -1,
+            x: -2,
             y: 0,
             layer: 1,
         },
@@ -34,22 +37,30 @@ fn place_characters(mut commands: Commands, asset_server: Res<AssetServer>) {
         },
     ));
 
-    commands.spawn((
-        Enemy,
-        Position {
-            x: 1,
-            y: 0,
-            layer: 1,
-        },
-        SpriteBundle {
-            texture: asset_server.load("goblin.png"),
-            ..default()
-        },
-    ));
+    let enemy_position = Position {
+        x: 2,
+        y: 0,
+        layer: 1,
+    };
+    let enemy_id = commands
+        .spawn((
+            Enemy,
+            enemy_position,
+            SpriteBundle {
+                texture: asset_server.load("goblin.png"),
+                ..default()
+            },
+        ))
+        .id();
+
+    let mut enemies = Enemies(HashMap::default());
+    enemies.insert(enemy_position, enemy_id);
+    commands.insert_resource(enemies);
 }
 
 fn move_player(
     keyboard_input: Res<Input<KeyCode>>,
+    mut enemies: ResMut<Enemies>,
     mut player_query: Query<&mut Position, With<Player>>,
     mut enemy_query: Query<&mut Position, (Without<Player>, With<Enemy>)>,
 ) {
@@ -66,17 +77,38 @@ fn move_player(
 
     let Some((move_x, move_y)) = movement else { return };
     let mut player = player_query.single_mut();
-    player.x += move_x;
-    player.y += move_y;
+    let move_position = Position {
+        x: player.x + move_x,
+        y: player.y + move_y,
+        layer: player.layer,
+    };
+
+    if let Some(_enemy_id) = enemies.get(&move_position) {
+        println!("attack enemy");
+    } else {
+        *player = move_position;
+    }
 
     for mut enemy in enemy_query.iter_mut() {
+        let mut enemy_move_position = enemy.clone();
         let horr_distance = (player.x - enemy.x).abs();
         let ver_distance = (player.y - enemy.y).abs();
 
-        if horr_distance > 1 && horr_distance >= ver_distance {
-            enemy.x += (player.x - enemy.x).cmp(&0) as i32;
-        } else if ver_distance > 1 {
-            enemy.y += (player.y - enemy.y).cmp(&0) as i32;
+        if ver_distance >= horr_distance {
+            enemy_move_position.y += (player.y - enemy.y).cmp(&0) as i32;
+        } else {
+            enemy_move_position.x += (player.x - enemy.x).cmp(&0) as i32;
+        }
+
+        if enemy_move_position == *player {
+            println!("attack player");
+        } else {
+            let Some((_, enemy_id)) = enemies.remove_entry(&enemy) else {
+                dbg!("enemy not in resource");
+                return;
+            };
+            *enemy = enemy_move_position;
+            enemies.insert(*enemy, enemy_id);
         }
     }
 }
@@ -91,7 +123,8 @@ fn position_to_translation(
 
 impl Plugin for BattlePlugin {
     fn build(&self, app: &mut App) {
-        app.add_system(place_characters.in_schedule(OnEnter(GameState::Battle)))
+        app.insert_resource(Enemies(HashMap::default()))
+            .add_system(place_characters.in_schedule(OnEnter(GameState::Battle)))
             .add_systems(
                 (
                     move_player.run_if(on_timer(Duration::from_millis(200))),
